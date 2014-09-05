b74e0900c49fd1f137f53a890c85bd2f38541c54
Nic Flores <flores314@yahoo.com>
2014-09-04 22:16:42 -0600

    fixed an extra simicolon in UserMethods

--- src/main/java/com/nicflores/app/UserMethods.java
+++ src/main/java/com/nicflores/app/UserMethods.java
@@ -4,10 +4,7 @@ package com.nicflores.app;
  * Created by nicflores on 9/3/14.
  */
 public interface UserMethods {
-;
     public void setUsername(String username);
     public String getPassword();
     public String getUsername();
-
-
 }
